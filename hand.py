acd1a8b0bf115d7c7b91f43d3bbff457f715c7de
InsectCondo <155114579+InsectCondo@users.noreply.github.com>
2024-01-21 19:25:02 +0100

    add function comments

--- hand.py
+++ hand.py
@@ -1,17 +1,34 @@
 from deck import Deck
+
 class Hand:
     def __init__(self, deck:Deck) -> None:
-        self.deck = deck
-        self.tiles = []
+        self.deck       = deck
+        self.tiles      = []
+        self.handsize   = 13
         self.replenish()
         pass
 
     def replenish(self):
-        while len(self.tiles) < 13:
+        """
+        pulls tiles from top of linked deck until the hand has enough of them
+        """
+        while len(self.tiles) < self.handsize:
             self.tiles.append(self.deck.pulltile())
     
     def __str__(self) -> str:
+        """
+        writes the unicode characters for each tile
+        
+        example:
+        `🀇 🀚 🀕 🀓 🀌 🀄 🀔 🀒 🀟 🀞 🀀 🀉 🀆
+        `"""
         return " ".join([str(tile) for tile in self.tiles])
 
     def name(self) -> str:
+        """
+        writes the names of each tile in english
+        
+        example:
+        `one of characters, two of circles, six of bamboo, four of bamboo, six of characters, red dragon, five of bamboo, three of bamboo, seven of circles, six of circles, east wind, three of characters, white dragon
+        `"""
         return ", ".join([tile.name() for tile in self.tiles])
--- tile.py
+++ tile.py
@@ -1,5 +1,6 @@
 from os.path import dirname
 DIR = dirname(__file__)
+
 class Tile:
     with open(f"{DIR}/tilenames.txt", "r") as file:
         tilenames = file.read().split("\n")
@@ -9,7 +10,17 @@ class Tile:
         pass
 
     def name(self) -> str:
+        """
+        writes the english name of the tile
+        
+        example: `red dragon`
+        """
         return self.tilenames[self.id]
     
     def __str__(self) -> str:
+        """
+        writes the unicode character of the tile
+        
+        example: `🀄`
+        """
         return chr(0x1F000 + self.id)
